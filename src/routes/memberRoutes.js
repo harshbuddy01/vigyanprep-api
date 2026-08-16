1e4341cfbb296439930a11cc06596825a5d1bb5b
harshbuddy01 <harshbuddy01@users.noreply.github.com>
2026-08-16 11:42:10 +0530

    security: enforce verifyAdminAuth middleware across all member management routes

--- src/routes/memberRoutes.js
+++ src/routes/memberRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { getMembers, addMember } from '../controllers/memberController.js';
+import { verifyAdminAuth } from '../middlewares/adminAuth.js';
 
 const router = express.Router();
+
+router.use(verifyAdminAuth);
 
 router.get('/', getMembers);
 router.post('/', addMember);
